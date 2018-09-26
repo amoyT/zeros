64e720326af1f7e1f9b33a4dcb53e312335a66cb
Tyoma Zhytsko <zhytsko.a@gmail.com>
2018-09-26 23:07:54 +0300

    Function was implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let result = 0;
+  while (number > 0){
+    number /= 5;
+    result += Math.floor(number);
+  }
+  return result;
 }
